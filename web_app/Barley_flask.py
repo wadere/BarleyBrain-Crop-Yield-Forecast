556c1ff87772a56c0505b69d903c1cb5487dccf2
wadere <wadere@gmail.com>
2017-03-07 22:29:23 -0700

    web app tweek

--- web_app/Barley_flask.py
+++ web_app/Barley_flask.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
 import os
 import cPickle as pickle
 import requests
@@ -13,12 +14,17 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
-@app.route('/EDA/', methods=['GET,POST'])
-def score():
+@app.route('/capstone', methods=['GET'])
+def capstone():
+    return render_template('capstone.html')
+
+@app.route('/eda', methods=['GET'])
+def eda():
     return render_template('EDA_gov_yields.html')
 
-
-
+@app.route('/github', methods=['GET'])
+def github():
+    return render_template('../static/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
