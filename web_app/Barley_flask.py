578eb7eb0ff2f2f6f7247027abe84457434d0e4b
wadere <wadere@gmail.com>
2017-03-23 12:57:43 -0600

    web port fix

--- web_app/Barley_flask.py
+++ web_app/Barley_flask.py
@@ -48,4 +48,4 @@ def galvanize():
     return redirect('https://talent.galvanize.com')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, threaded=True)
+    app.run(host='0.0.0.0', threaded=True)
